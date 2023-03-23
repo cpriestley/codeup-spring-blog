43d990622580a8c8aa5744ee81eeb67f85c45790
Clayton Priestley <clayton.priestley@gmail.com>
2023-03-23 09:35:10 -0500

    initial commit - from local

---
+++ src/test/java/com/code/codeupspringblog/CodeupSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.codeupspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

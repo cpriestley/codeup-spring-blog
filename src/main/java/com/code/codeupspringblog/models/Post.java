3705bca3fea1e41e7863dd86f73635683fef3896
Clayton Priestley <clayton.priestley@gmail.com>
2023-03-25 23:20:50 -0500

    Implement PostController - add create view - add sample datasource and jpa config - add jQuery to partials and update 'create post' link to use PostController - add JPA spring boot starter - add Entity decorations to Post class - add PostRepository and inject into PostController - refactor PostController to use JPA instead of hard-coded posts

--- src/main/java/com/code/codeupspringblog/models/Post.java
+++ src/main/java/com/code/codeupspringblog/models/Post.java
@@ -1,12 +1,19 @@
 package com.code.codeupspringblog.models;
 
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
-@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(nullable = false, length = 150)
     private String title;
+    @Column(nullable = false, length = 5000)
     private String body;
 }

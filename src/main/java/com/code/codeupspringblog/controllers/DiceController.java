b854b0276275027555a03ca08734050d8cc32781
Clayton Priestley <clayton.priestley@gmail.com>
2023-03-27 12:06:51 -0500

    Update Dice View - replace dice values with images - move choices and results into tables - added 6 random dice rolls

--- src/main/java/com/code/codeupspringblog/controllers/DiceController.java
+++ src/main/java/com/code/codeupspringblog/controllers/DiceController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.random.RandomGenerator;
 
 @Controller
@@ -16,9 +18,12 @@ public class DiceController {
     }
 
     @GetMapping("/roll-dice/{guess}")
-    public String rollDice(@PathVariable String guess, Model model) {
-        int result = RandomGenerator.getDefault().nextInt(6) + 1;
-        model.addAttribute("result", result);
+    public String rollDice(@PathVariable int guess, Model model) {
+        List<Integer> diceRolls = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            diceRolls.add(RandomGenerator.getDefault().nextInt(6) + 1);
+        }
+        model.addAttribute("results", diceRolls);
         model.addAttribute("guess", guess);
         return "dice";
     }

c4c219357cc24f952bd287c74044203ea4df9e65
Clayton Priestley <clayton.priestley@gmail.com>
2023-03-29 15:43:55 -0500

    Add Inputs to Hello View - add form that utilizes /hello/${name} - add username of current user as default value

--- src/main/java/com/code/codeupspringblog/controllers/HelloController.java
+++ src/main/java/com/code/codeupspringblog/controllers/HelloController.java
@@ -1,14 +1,21 @@
 package com.code.codeupspringblog.controllers;
 
+import com.code.codeupspringblog.models.UserWithRoles;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String helloWorld() {
+    public String helloWorld(Model model) {
+        UserWithRoles user = (UserWithRoles) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
         return "hello";
     }
 
